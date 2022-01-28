9466fc5c55ad4fe45d1a94c55209aca379d726b9
PolyFlower <btchead@protonmail.ch>
2022-01-28 07:22:50 +0300

    User component added

---
+++ src/components/User.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import UserIcon from "./svg/UserIcon";
+
+type Props = {};
+
+const User = (props: Props) => {
+	return (
+		<div className="user-cnt">
+			<UserIcon />
+		</div>
+	);
+};
+
+export default User;
