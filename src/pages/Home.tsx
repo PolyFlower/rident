b6d3c380b01c0274f11d96afff81544ed1e2e22d
PolyFlower <btchead@protonmail.ch>
2022-01-29 16:28:48 +0300

    Pages for router added

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+
+type Props = {};
+
+const Home = (props: Props) => {
+	return (
+		<>
+			<Header />
+		</>
+	);
+};
+
+export default Home;
