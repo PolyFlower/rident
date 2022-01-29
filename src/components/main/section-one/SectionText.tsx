a575393cd4009c488d7b795c2c8d6ee3ac048290
PolyFlower <btchead@protonmail.ch>
2022-01-29 16:34:18 +0300

    SectionText component added

---
+++ src/components/main/section-one/SectionText.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+const SectionText = (props: Props) => {
+	return (
+		<div className="section-one-text">
+			<span className="one">Find the best deal!</span>
+			<span className="two">Choose from wide range of options.</span>
+		</div>
+	);
+};
+
+export default SectionText;
