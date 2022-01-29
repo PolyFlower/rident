285fd588a25e514b4fb5d21496854fc8c11c1bdd
PolyFlower <btchead@protonmail.ch>
2022-01-29 16:29:57 +0300

    Router initial implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Header from "./components/Header";
+import Main from "./components/Main";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
 
 type Props = {};
 
 const App = (props: Props) => {
 	return (
 		<>
-			<Header />
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="offers" element={<Offers />}></Route>
+			</Routes>
 		</>
 	);
 };
