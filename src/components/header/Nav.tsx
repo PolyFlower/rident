66d01e26918017b961d70fd3a432c28cb125e622
PolyFlower <btchead@protonmail.ch>
2022-01-30 09:42:49 +0300

    Minor fix in Link element

--- src/components/header/Nav.tsx
+++ src/components/header/Nav.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const Nav = (props: Props) => {
 	return (
 		<nav>
-			<NavButton href="offers" label="Offers" />
+			<NavButton href="/offers" label="Offers" />
 			<NavButton href="/" label="Locations" />
 			<NavButton href="/" label="Reviews" />
 		</nav>
