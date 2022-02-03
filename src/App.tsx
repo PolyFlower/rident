908fcd35a671a16f80bc746344158a670437dfe1
PolyFlower <btchead@protonmail.ch>
2022-02-03 12:39:07 +0300

    Temporarily removed offers path

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import React from "react";
-import Header from "./components/Header";
-import Main from "./components/Main";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Offers from "./pages/Offers";
 
-type Props = {};
-
-const App = (props: Props) => {
+const App = (props: any) => {
 	return (
 		<>
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="offers" element={<Offers />} />
 			</Routes>
 		</>
 	);
