c626269457e8fa768767f1ec390ec8267c0818be
Roman <btchead@protonmail.ch>
2022-02-09 17:55:00 +0300

    Static breadcrumb

--- src/components/main/subnav/BreadCrumbs.tsx
+++ src/components/main/subnav/BreadCrumbs.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const BreadCrumbs = (props: any) => {
-	const currentPath = useLocation();
-
 	return (
 		<nav className="bread-crumb">
-			<Link to={currentPath.pathname} className="bread-crumb__item">
+			<Link to="/" className="bread-crumb__item">
 				<span className="bread-crumb__cnt">Home</span>
 			</Link>
-			<Link to={currentPath.pathname} className="bread-crumb__item active">
+			<Link to="/browse" className="bread-crumb__item active">
 				<span className="bread-crumb__cnt">Browse</span>
 			</Link>
 		</nav>
